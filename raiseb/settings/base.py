e487db2151ea985b33a65ca2fc2e9ac614f022b8
Vipul Kumar <66182594+Vipul208@users.noreply.github.com>
2022-12-31 21:18:38 +0530

    server bug fixing

--- raiseb/settings/base.py
+++ raiseb/settings/base.py
@@ -23,10 +23,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(
 SECRET_KEY = 'django-insecure-zd)v(9jms!aqf-&6v8!f=a@*q15o_=3fe6nr#jt%902ex!41$#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ["*"]
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -81,12 +77,6 @@ WSGI_APPLICATION = 'raiseb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
--- raiseb/urls.py
+++ raiseb/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path("ckeditor/", include("ckeditor_uploader.urls")),
     path("blog", include("blog.urls")),
 ]
+
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
