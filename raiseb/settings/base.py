a89ba71db17c620ec3ecdf0bdd4592771ca9f4a1
Vipul Kumar <66182594+Vipul208@users.noreply.github.com>
2023-01-12 13:09:44 +0530

    bug fix 20

--- raiseb/settings/base.py
+++ raiseb/settings/base.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-zd)v(9jms!aqf-&6v8!f=a@*q15o_=3fe6nr#jt%902ex!41$#'
 
-DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
